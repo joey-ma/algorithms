0bc305b42cb8e7aae8eba298f6e0927fd85b2f80
yoyoyojoe <jpma.family@gmail.com>
2022-11-13 21:37:20 -0800

    added can-sum.js solutions

--- can-sum.js
+++ can-sum.js
@@ -8,12 +8,65 @@ You may use an element of the array as many times as needed.
 
 You may assume that all input numbers are nonnegative. */
 
+// canSum, recursive solution
 const canSum = (targetSum, numbers) => {
-  const obj = {};
-  if (num === 0) return true;
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
 
-  numbers.forEach((num) => {
-    if (obj[num] !== undefined) return canSum(num, numbers);
-    obj[num] = targetSum - num;
-  });
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    if (canSum(remainder, numbers)) return true;
+  }
+
+  return false;
 };
+
+// canSum, recursive solution memoized
+const canSumMemoized = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    if (canSum(remainder, numbers)) return true;
+  }
+
+  return false;
+};
+
+console.log(canSumMemoized(7, [2, 3])); // true
+console.log(canSumMemoized(7, [5, 3, 4, 7])); // true
+console.log(canSumMemoized(7, [2, 4])); // false
+console.log(canSumMemoized(7, [2, 3, 5])); // true
+console.log(canSumMemoized(300, [7, 14])); // false
+
+// todo: canSum, iterative solution... not solved yet
+const canSumIterative = (targetSum, numbers) => {
+  const obj = {};
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+
+    if (remainder in obj) return obj[remainder];
+    if (remainder === 0) {
+      obj[remainder] = true;
+      return true;
+    }
+
+    if (remainder < 0) {
+      obj[remainder] = false;
+      return false;
+    }
+
+    // return canSumIterative(remainder, numbers);
+  }
+
+  return false;
+};
+
+console.log(canSumIterative(7, [2, 3])); // true
+console.log(canSumIterative(7, [5, 3, 4, 7])); // true
+console.log(canSumIterative(7, [2, 4])); // false
+console.log(canSumIterative(7, [2, 3, 5])); // true
+console.log(canSumIterative(300, [7, 14])); // false
